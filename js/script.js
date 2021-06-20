fcc97c9a94770e91f91f31c9578397f0a94ec20f
RedMadda <maddalenarossaro91@gmail.com>
2021-06-20 12:19:52 +0200

    e finalmente viene anche con filter

--- js/script.js
+++ js/script.js
@@ -7,22 +7,40 @@ const nani = ["Brontolo", "Eolo", "Pisolo", "Cucciolo", "Mammolo", "Dotto", "Gon
 // creo allocazione nani filtrati
 const naniFiltrati = [];
 // prendo le due posizioni
-const index1 = parseInt(prompt("Inserisci un numero fra 0 e 6"));
-const index2 = parseInt(prompt("Inserisci un altro numero maggiore o uguale a quello di prima"));
+let index1 = parseInt(prompt("Inserisci un numero fra 0 e 6"));
+let index2 = parseInt(prompt("Inserisci un altro numero maggiore o uguale a quello di prima"));
 
 // procedo con forEach
-nani.forEach((nano, index) => {
-    if (index >= index1 && index <= index2){
-        naniFiltrati.push(nano);
+// nani.forEach((nano, index) => {
+//     if (index >= index1 && index <= index2){
+//         naniFiltrati.push(nano);
+//     }
+// });
+
+
+
+// console.log(
+//     `Il primo indice inserito è: ${index1};
+//      Il secondo indice inserito è: ${index2};
+//      L'array filtrato fra i due indexes è: ${naniFiltrati};
+//     `
+//     ); 
+
+// procedo con filter
+// array rimane lo stesso, lo "filtro";
+const naniFiltrati1 = nani.filter((nano, index) => {
+    if(index >= index1 && index <= index2){
+        let logInd = index + 1;
+        console.log(
+            `${logInd}) ${nani[index]};
+            `
+        );
+    }
+
+    if(!(index >= index1) || !(index <= index2)){
+        alert("Hai sbagliato nella scelta dei numeri! Riprova");
+        index1 = parseInt(prompt("Inserisci un numero fra 0 e 6"));
+        index2 = parseInt(prompt("Inserisci un altro numero maggiore o uguale a quello di prima"));
+
     }
 });
-
-
-
-console.log(
-    `Il primo indice inserito è: ${index1};
-     Il secondo indice inserito è: ${index2};
-     L'array filtrato fra i due indexes è: ${naniFiltrati};
-    `
-    ); 
-
