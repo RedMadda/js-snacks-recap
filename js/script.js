ee4d991c4eeff300b59bcba9cc0ee10b3fcc5d2b
RedMadda <maddalenarossaro91@gmail.com>
2021-08-15 15:34:40 +0200

    filter & forEach

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
-function getInterGear(array, min, max){
-    const filteredArray = array.filter((el, i)=>{
+function getInterGearFilter(array, min, max){
+    const filteredArray = array.filter((el, i) => i > min && i < max);
+    return filteredArray;
+}
+
+function getInterGearRForEach(array, min, max){
+    filteredArray = [];
+    array.forEach((el, i) =>{
         if(i > min && i < max){
-            return el;
+            filteredArray.push(el);
         }
     });
     return filteredArray;
@@ -14,4 +20,6 @@ function getInterGear(array, min, max){
 const nums = [0,1,2,3,4,5,6,7,8,9,10];
 
 
-console.log(getInterGear(nums, 1,5));
+console.log("Filter: ",getInterGearFilter(nums, 1,5));
+console.log("ForEach: ", getInterGearRForEach(nums, 5,8));
+
