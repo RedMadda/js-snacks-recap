ad723730c2300de535bf2b9253194878f26ab2b3
RedMadda <maddalenarossaro91@gmail.com>
2021-08-15 15:34:08 +0200

    filter + forEach

--- js/script.js
+++ js/script.js
@@ -2,47 +2,16 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
-// creo array da filtrare
-const nani = ["Brontolo", "Eolo", "Pisolo", "Cucciolo", "Mammolo", "Dotto", "Gongolo"];
-// creo allocazione nani filtrati
-let naniFiltrati = [];
-// prendo le due posizioni
-let index1 = parseInt(prompt("Inserisci un numero fra 0 e 6"));
-let index2 = parseInt(prompt("Inserisci un altro numero maggiore o uguale a quello di prima"));
-
-// function filtriamo(array, num1, num2){
-// // ** procedo con forEach
-// array.forEach((nano, index) => {
-//     if (index >= num1 && index <= num2){
-//         naniFiltrati.push(nano);
-//     }
-// });
-// }
-
-// filtriamo(nani,index1,index2);
-
-
-// console.log(
-//     `Il primo indice inserito è: ${index1};
-//      Il secondo indice inserito è: ${index2};
-//      L'array filtrato fra i due indexes è: ${naniFiltrati};
-//     `
-//     ); 
-
-// **procedo con filter
-function filtraPerIndici(array, num1, num2){
-
-    const arrayFiltrato = array.filter((element, index) => {
-
-        if(index >= num1 && index <= num2){
-            return element;
-        } else if(num2 < num1){
-            alert("Errore nella scelta dei numeri, num2 è minore di num1. Ricarica e riprova.")
+function getInterGear(array, min, max){
+    const filteredArray = array.filter((el, i)=>{
+        if(i > min && i < max){
+            return el;
         }
-
     });
-    //ho trovato il mio arrayFiltrato. Lo visualizzo:
-    document.getElementById("print").innerHTML += "Il/i tuo/i nano/i è/sono: " + arrayFiltrato;
+    return filteredArray;
 }
 
-filtraPerIndici(nani,index1,index2);
+const nums = [0,1,2,3,4,5,6,7,8,9,10];
+
+
+console.log(getInterGear(nums, 1,5));
